c91ad60df88d397deda5a19641640310c7db5243
reacher <reacher@ubuntu20.mele.com.cn>
2015-07-09 09:21:50 +0800

    test repo

---
+++ repo_test/skin.cood-e/skin.coode/extras/scripts/update.py
@@ -0,0 +1,16 @@
+import xbmc, xbmcgui
+import os
+import xbmcaddon
+
+cmd = os.path.join(xbmc.translatePath(xbmcaddon.Addon('script.tlbb').getAddonInfo('path')), 'update.py')
+cmd += ',0'
+
+
+dialog = xbmcgui.Dialog()
+if dialog.yesno('COOD-E UPDATE','', 'Do you want to check for Firmware update'):
+
+
+  xbmc.executebuiltin("XBMC.RunScript(%s)"%cmd)
+
+
+
